cc463b2b74def452acb1d1f30f7f27b9fe76507b
elroi <elroinoronha2@gmail.com>
2021-11-19 16:09:02 +0530

    added a file to secrets folder, testing it

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth"
-import firebaseConfig from "../Config/firebaseConfig";
+import firebaseConfig from "../secrets/firebaseConfig";
 
   // Initialize Firebase
 const app = initializeApp(firebaseConfig);
